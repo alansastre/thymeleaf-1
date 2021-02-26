47a64cfa8d393091a2ef8d2f53bd9b0e10894243
JorgePallolTrigueros <jorgepallol1@gmail.com>
2021-02-26 10:33:01 +0100

    Paginacion

--- src/main/java/com/example/demo/controller/MovieController.java
+++ src/main/java/com/example/demo/controller/MovieController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
+import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +30,8 @@ import com.example.demo.entities.User;
 import com.example.demo.repository.FilmProducerRepository;
 import com.example.demo.repository.GenreRepository;
 import com.example.demo.repository.MovieRepository;
+import com.example.demo.services.MovieSrviceAPI;
+
 
 @Controller
 public class MovieController {
@@ -104,5 +119,26 @@ public class MovieController {
 		return "movie-list";
 		
 	}
+	
+	
+	@Autowired
+	private MovieSrviceAPI movieServiceAPI;
+	
+	@GetMapping(value = "/moviepaginada")
+	public String finnAll (@RequestParam Map <String, Object> params, Model model){
+		int page = params.get("page") !=null ? (Integer.valueOf(params.get("page").toString()) - 1) : 0;
+		PageRequest pageRequest = PageRequest.of(page, 10);
+		Page<Movie> pageCustomer = movieServiceAPI.gettAll(pageRequest);
+		
+		int totalPage =  pageCustomer.getTotalPages();
+		if (totalPage >0) {
+			List<Integer> pages = IntStream.rangeClosed(1, totalPage).boxed().collect(Collectors.toList());		
+			model.addAttribute("pages", pages);		
+		}
+		model.addAttribute("list", pageCustomer.getContent());
+
+		return "moviepaginada";
+	}
+	
 	
 }
