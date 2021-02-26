9797e3c52bc4f32caa6e95779f98832de5e5f131
Leticia Orive Peña <letimedi17031994@gmail.com>
2021-02-26 11:58:39 +0100

    entities categoriacinema y movie

--- src/main/java/com/example/demo/entities/Cinema.java
+++ src/main/java/com/example/demo/entities/Cinema.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -37,81 +38,130 @@ import javax.persistence.Table;
 		
 		@ManyToMany(mappedBy="cinemas")
 		private List<Movie> movies = new ArrayList<Movie>();
-		
+		@ManyToMany
+		@JoinTable(name = "categorycinema",
+		joinColumns = @JoinColumn(name = "cinema_id", referencedColumnName = "id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id"))
+		private List<CategoryCinema> categories = new ArrayList<>();
 		public Cinema() {}
 
-		public Cinema(String name,  String schedule, Integer rooms, Integer postalCode,
-				List<Movie> movies, List<User> user) {
+		
+
+		public Cinema(String name, String schedule, Integer time, Integer rooms, Integer postalCode, List<Movie> movies,
+				List<CategoryCinema> categories) {
 			super();
 			this.name = name;
-			
 			this.schedule = schedule;
+			this.time = time;
 			this.rooms = rooms;
 			this.postalCode = postalCode;
 			this.movies = movies;
-			
+			this.categories = categories;
 		}
 
-		public Integer getPostalCode() {
-			return postalCode;
-		}
 
-		public void setPostalCode(Integer postalCode) {
-			this.postalCode = postalCode;
-		}
 
 		public Long getId() {
 			return id;
 		}
 
+
+
 		public void setId(Long id) {
 			this.id = id;
 		}
+
+
 
 		public String getName() {
 			return name;
 		}
 
+
+
 		public void setName(String name) {
 			this.name = name;
 		}
 
-		
 
-		public Integer getRooms() {
-			return rooms;
-		}
 
-		public void setRooms(Integer rooms) {
-			this.rooms = rooms;
-		}
-
-		public List<Movie> getMovies() {
-			return movies;
-		}
-
-		public void setMovies(List<Movie> movies) {
-			this.movies = movies;
-		}
-		
 		public String getSchedule() {
 			return schedule;
 		}
+
+
 
 		public void setSchedule(String schedule) {
 			this.schedule = schedule;
 		}
 
-		
+
+
+		public Integer getTime() {
+			return time;
+		}
+
+
+
+		public void setTime(Integer time) {
+			this.time = time;
+		}
+
+
+
+		public Integer getRooms() {
+			return rooms;
+		}
+
+
+
+		public void setRooms(Integer rooms) {
+			this.rooms = rooms;
+		}
+
+
+
+		public Integer getPostalCode() {
+			return postalCode;
+		}
+
+
+
+		public void setPostalCode(Integer postalCode) {
+			this.postalCode = postalCode;
+		}
+
+
+
+		public List<Movie> getMovies() {
+			return movies;
+		}
+
+
+
+		public void setMovies(List<Movie> movies) {
+			this.movies = movies;
+		}
+
+
+
+		public List<CategoryCinema> getCategories() {
+			return categories;
+		}
+
+
+
+		public void setCategories(List<CategoryCinema> categories) {
+			this.categories = categories;
+		}
+
+
 
 		@Override
 		public String toString() {
-			return "Cinema [id=" + id + ", name=" + name + ", schedule=" + schedule + ", rooms=" + rooms + ", postalCode="
-					+ postalCode + "]";
+			return "Cinema [id=" + id + ", name=" + name + ", schedule=" + schedule + ", time=" + time + ", rooms="
+					+ rooms + ", postalCode=" + postalCode + "]";
 		}
 
-		
-		
-		
 
 	}
